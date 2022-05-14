619df490511cb2ed37a89b8d0274695ba4a03bd4
Babith Ganesh <babithg@gmail.com>
2022-05-14 10:49:00 +0530

    modified the app file and style templates

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/favicon.ico')
+def fevicon():
+    return True
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
